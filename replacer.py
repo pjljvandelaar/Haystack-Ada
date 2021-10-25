a219aa5e9b157527d09797d33e54868e8762f1d3
ksakacep <konrad.socha10@wp.pl>
2021-10-25 16:21:59 +0200

    worked on the replacer, some test cases

--- replacer.py
+++ replacer.py
@@ -1,21 +1,18 @@
 import libadalang as lal
 from typing import Optional, Union
 from searchresult import SearchResult
-import ReplaceMain as replacer
 
 
 class Replacer:
-    def __init__(self, filename: str, slocs: list, replacement: str, indexes: Optional[Union[list, None]] = None):
+    def __init__(self, filename: str, slocs: list, replacement: str, indexes: Optional[Union[list, None]] = None, output: Optional[Union[str, None]] = None):
         """Constructor method
         """
         self.slocs = slocs
         self.replacement = replacement
         self.filename = filename
         self.locations = self.parse_sloc()
-        if indexes is None:
-            self.indexes = [i for i in range(len(self.locations))]
-        else:
-            self.indexes = indexes
+        self.indexes = [i for i in range(len(self.locations))] if indexes is None else indexes
+        self.output = filename if output is None else output
 
     def parse_sloc(self):
         locations = []
@@ -26,30 +23,71 @@ class Replacer:
         return locations
 
     def replace(self) -> None:
-        for i in self.indexes[::-1]:
+        with open(self.filename, "r") as infile:
+            lines = infile.readlines()
+        parts = []
+        for i, j in enumerate(self.indexes):
+            start_line = int(self.locations[j][0][0])
+            end_line = int(self.locations[j][1][0])
+            start_char = int(self.locations[j][0][1])
+            end_char = int(self.locations[j][1][1])
+            if i == 0:
+                parts.append(lines[:start_line-1])
+                parts[-1].append(lines[start_line-1][:start_char-1])
+            else:
+                previous_end_line = int(self.locations[self.indexes[i - 1]][1][0])
+                previous_end_char = int(self.locations[self.indexes[i - 1]][1][1])
+                parts.append([lines[previous_end_line - 1][previous_end_char:]])
+                parts[-1].extend(lines[previous_end_line:start_line - 1])
+                parts[-1].append(lines[start_line - 1][:start_char - 1])
+            if i == len(self.indexes) - 1:
+                parts.append([lines[end_line-1][end_char:]])
+                parts[-1].extend(lines[end_line:])
+        output_str = ""
+        for idx, part in enumerate(parts):
+            for line in part:
+                output_str += line
+            if idx == len(parts) - 1:
+                break
+            output_str += self.replacement
+        print(len(parts))
+        print(*parts, sep='\n')
+        with open(self.output, "w") as outfile:
+            outfile.write(output_str)
+        '''
             print(self.locations[i])
-            linestart = int(self.locations[i][0][0])
-            linend = int(self.locations[i][1][0])
+
             # print(linestart,linend)
             # Equals line:
-            if(linestart == linend):
+            if linestart == linend:
                 startChar = int(self.locations[i][0][1])
                 endChar = int(self.locations[i][1][1])
-                replacer.replace_line(self.filename,linestart,startChar,"I Test Replacer!",endChar)
+                replacer.replace_line(self.filename,linestart,startChar,self.replacement,endChar, output=self.output)
             # No equals line (ex: for loop...)
-            elif(linestart != linend):
+            elif linestart != linend:
                 startChar = int(self.locations[i][0][1])
-                replacer.replace_body(self.filename,linestart,linend,["Put_Line(firstLine);\n","Put_Line(secondLine);\n","Put_Line(thirdLine);\n"],startChar)
-        print("Out of the loop")
+                replacer.replace_body(self.filename,linestart,linend,["Put_Line(firstLine);\n","Put_Line(secondLine);\n","Put_Line(thirdLine);\n"],startChar, output=self.output)
+        # print("Out of the loop")
+        '''
 
-
+'''
 res = SearchResult("hello.adb", "Put(Arr(I).X)", rule=lal.GrammarRule.expr_rule)
 # res = SearchResult("hello.adb", "\"Hello World\"", rule=lal.GrammarRule.expr_rule)
-'''res = SearchResult("hello.adb", "if The_Month > The_Time then\n"
+res = SearchResult("hello.adb", "if The_Month > The_Time then\n"
                                         "The_Month := The_Time;\n"
                                         "end if;",
                               rule=lal.GrammarRule.if_stmt_rule)
-'''
+
 test = Replacer(res.filename, res.locations, "test")
 test.replace()
-
+'''
+res = SearchResult("dosort.adb", "for I     in Arr'Range loop\n\n"
+                                     "Put(\"(\") ;\n"
+                                     "Put($array);\n"
+                                     "Put(\",\");\n"
+                                     "Put(Arr(I).Y);  \n"
+                                     "Put(\")\");\n\n\n"
+                                     "end loop;\",",
+                       rule=lal.GrammarRule.loop_stmt_rule)
+test = Replacer(res.filename, res.locations, "test1!\ntest2!\nworks?\n", output="replacer_test.adb")
+test.replace()
--- searchresult_test.py
+++ searchresult_test.py
@@ -4,12 +4,13 @@ import libadalang as lal
 
 
 class TestSearchResult(unittest.TestCase):
-    def test_result(self):
+    def test_expr_rule(self):
         # standard put statement
         dosort1 = SearchResult("dosort.adb", "Put(Arr(I).X)", rule=lal.GrammarRule.expr_rule)
         self.assertEqual(dosort1.found, True)
         self.assertEqual(str(dosort1.locations), "[<SlocRange 34:10-34:23>]")
 
+    def test_for_loop(self):
         # for loop
         dosort2 = SearchResult("dosort.adb", "for I     in Arr'Range loop\n\n"
                                              "Put(\"(\") ;\n"
@@ -22,26 +23,30 @@ class TestSearchResult(unittest.TestCase):
         self.assertEqual(dosort2.found, True)
         self.assertEqual(str(dosort2.locations), "[<SlocRange 32:7-38:16>]")
 
+    def test_empty(self):
         # empty search term
         dosort3 = SearchResult("dosort.adb", "",
                                rule=lal.GrammarRule.compilation_rule)
         self.assertEqual(dosort3.found, False)
         self.assertEqual(str(dosort3.locations), "[]")
 
+    def test_invalid_file(self):
         # invalid filename
         with self.assertRaises(ValueError):
             SearchResult("", "", rule=lal.GrammarRule.compilation_rule)
 
+    def test_exception_file(self):
         # exception rule
         stack1 = SearchResult("g_stack_user.adb", "when Data_Error =>\n"
                                                   "Put_Line(\"Okay, that'll be your first string.\");\n",
                               rule=lal.GrammarRule.exception_handler_rule)
         self.assertEqual(stack1.found, True)
 
+    def test_if(self):
         # if statement
         obj1 = SearchResult("obj1.adb", "if LC.Count > L\n"
                                         "then LC.Count := L;\n"
                                         "end if;",
-                              rule=lal.GrammarRule.if_stmt_rule)
+                            rule=lal.GrammarRule.if_stmt_rule)
         self.assertEqual(obj1.found, True)
         self.assertEqual(str(obj1.locations), "[<SlocRange 38:7-38:50>]")

8656e7d4bbae313294409b52b93b2e38f130449a
ksakacep <konrad.socha10@wp.pl>
2021-11-22 16:13:29 +0100

    wildcard support in the location class

--- location.py
+++ location.py
@@ -1,10 +1,20 @@
+from typing import Dict
+
+
 class Location:
-    def __init__(self, start_line, end_line, start_char, end_char):
+    start_line: int
+    end_line: int
+    start_char: int
+    end_char: int
+    wildcards: Dict[str, str]
+
+    def __init__(self, start_line: int, end_line: int, start_char: int, end_char: int, wildcards: Dict[str, str]):
         self.start_line = start_line
         self.end_line = end_line
         self.start_char = start_char
         self.end_char = end_char
+        self.wildcards = wildcards
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.start_line) + ":" + str(self.start_char) + "-" \
             + str(self.end_line) + ":" + str(self.end_char)
